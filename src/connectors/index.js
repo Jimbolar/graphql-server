d9f3fed8854d366f70555c9b0494d8bbfe5dcd8c
Jimbolar <james.robinson@sky.uk>
2018-07-21 00:52:20 +0100

    updating connectors

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -2,12 +2,23 @@ import fetch from 'node-fetch';
 
 const fortniteStats = {
   getStats: function() { /* method of the fortniteStats object */
-    return fetch('https://api.fortnitetracker.com/v1/profile/psn/jimbolar')
-      .then(res => res.json())
-      .then(res => {
-        return res[0].epicUserHandle;
-      });
-  },
-};
+    return fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
+          .then(res => res.json())
+          .then(res => {return res[0].fortune.message;})
+    },
+  };
 
 export { fortniteStats };
+
+
+// return fetch('https://api.fortnitetracker.com/v1/profile/psn/jimbolar')
+//   .then(res => res.json())
+//   .then(res => {return res[0].epicUserHandle;})
+
+
+        //     method: 'GET',
+        //     body: JSON.stringify(body),
+        //     headers: {
+        //       'Content-Type': 'application/json',
+        //       'TRN-Api-Key': '405452a2-a7ab-4973-8aa8-72db714fb1f1' /* fortnite api key */
+        // }),
